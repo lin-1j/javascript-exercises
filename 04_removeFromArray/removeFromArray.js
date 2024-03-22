a51f2cbd6f98bb939007577665c328459b369697
Jackey2baccey <96613933+Jackey2baccey@users.noreply.github.com>
2024-03-22 20:47:31 +1100

    Edit removeFromArray function

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...input) {
+    let new_array = [];
 
+    array.forEach(function(item){
+        if (!input.includes(item)) {
+            new_array.push(item);
+        }
+    });
+
+    return new_array;
 };
+
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
