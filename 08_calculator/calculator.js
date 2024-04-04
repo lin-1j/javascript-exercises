d51f17aa6cc51f35a0b93f8b5d7fab428d613d53
Jackey2baccey <96613933+Jackey2baccey@users.noreply.github.com>
2024-04-04 15:41:21 +1100

    Edit functions in calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,48 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+  return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+  return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  return array.reduce((total, value) => {
+    return total + value;
+  }, 0)
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((total, value) => {
+    return total * value;
+  })
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+  if (num2 === 0) {
+    return 1;
+  } else if (num2 === 1) {
+    return num1;
+  } else {
+    let result = 1;
+    for (let i = 0; i < num2; i++) {
+      result *= num1;
+    }
+    return result;
+  }
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  if (num === 0) {
+    return 1;
+  }
+
+	let product = 1;
+  for (let i = num; i > 0; i--){
+    product *= i;
+  }
+
+  return product;
 };
 
 // Do not edit below this line
