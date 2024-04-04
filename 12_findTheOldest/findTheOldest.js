4731feb4f17cb9a9aefa3933069d309b83e6159c
Jackey2baccey <96613933+Jackey2baccey@users.noreply.github.com>
2024-04-04 16:38:27 +1100

    Edit findTheOldest function

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,17 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(array) {
+    return array.reduce((oldest, current) => {
+        let oldestAge = getAge(oldest.yearOfDeath, oldest.yearOfBirth);
+        let currentAge = getAge(current.yearOfDeath, current.yearOfBirth);
+        return oldestAge > currentAge ? oldest : current;
+    })
 };
 
+function getAge(death, birth) {
+    if (!death) {
+        death = new Date().getFullYear();
+    } 
+
+    return death - birth;
+}
 // Do not edit below this line
 module.exports = findTheOldest;
