3194b6282bea66d762a2cf71b180365cfb89f8e3
Jackey2baccey <96613933+Jackey2baccey@users.noreply.github.com>
2024-04-04 16:19:44 +1100

    Edit fibonacci function

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,23 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    if (typeof num !== 'number') {
+        num = parseInt(num);
+    }
 
+    if (num === 0) {
+        return 0;
+    } else if (num === 1) {
+        return 1;
+    } else if (num <= 0) {
+        return "OOPS";
+    }
+
+    let fib = [0, 1];
+
+    for (let i = 2; i <= num; i++) {
+        fib[i] = fib[i - 2] + fib[i - 1];
+    }
+
+    return fib[num];
 };
 
 // Do not edit below this line
